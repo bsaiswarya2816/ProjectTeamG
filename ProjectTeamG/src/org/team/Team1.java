b44f5743cd6515de1ec007434344e92a37b7bada
bsaiswarya2816 <bsaiswarya2816@gmail.com>
2023-11-26 12:18:38 +0530

    Initial Commit

---
+++ ProjectTeamG/src/org/team/Team1.java
@@ -0,0 +1,22 @@
+package org.team;
+
+public class Team1 {
+	
+	private void throwBall() {
+		System.out.println("This is ThrowBall");
+
+	}
+	private void handBall() {
+		System.out.println("This is HandBall");
+
+	}
+	private void footBall() {
+		System.out.println("This is FootBall");
+
+	}
+	private void volleyBall() {
+		System.out.println("This is VolleyBall");
+
+	}
+
+}
